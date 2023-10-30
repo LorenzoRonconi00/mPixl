64c1edb5f4e6ade2771109ef65a326916564be56
Federicocervelli <federicocervelli01@gmail.com>
2023-10-30 22:23:32 +0100

    sasadasd

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,50 +1,56 @@
 import { motion, useTransform, useScroll } from "framer-motion";
 import { useRef } from "react";
 import Parallax from "../parallax/Parallax";
+import "./parallax.scss";
 
 export const cards = [
-    {
-        id: 1,
-        title: "SNL Shipping",
-        img: "/snlshipping.png",
-        desc: "Sito web per azienda di trasporti",
-        url: "http://snlshipping.com/"
-    },
-    {
-        id: 2,
-        title: "Hotel Marittima",
-        img: "/marittima.png",
-        desc: "Sito web per hotel",
-        url: "https://marittima.vercel.app/"
-    },
-    {
-        id: 3,
-        title: "MecSpare",
-        img: "/mecspare.png",
-        desc: "Sito web per rivendita di ricambi online",
-        url: "https://mecspare2.vercel.app/"
-    },
-  ];
+  {
+    id: 1,
+    title: "SNL Shipping",
+    img: "/snlshipping.png",
+    desc: "Sito web per azienda di trasporti",
+    url: "http://snlshipping.com/",
+  },
+  {
+    id: 2,
+    title: "Hotel Marittima",
+    img: "/marittima.png",
+    desc: "Sito web per hotel",
+    url: "https://marittima.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "MecSpare",
+    img: "/mecspare.png",
+    desc: "Sito web per rivendita di ricambi online",
+    url: "https://mecspare2.vercel.app/",
+  },
+];
 
 const Portfolio = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: targetRef,
   });
-
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
   const x = useTransform(scrollYProgress, [0, 1], ["1%", "-50%"]);
 
   return (
+    
     <div ref={targetRef} className="relative h-[300vh] z-10">
-      <Parallax />
-      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+      <div className="sticky top-0 w-screen flex h-screen items-center overflow-hidden parallax">
+        
         <motion.div style={{ x }} className="flex gap-4">
           {cards.map((card) => {
             return <Card card={card} key={card.id} />;
           })}
         </motion.div>
+        
+        <motion.div className="mountains"></motion.div>
+        <motion.div className="stars" style={{ x: yBg }}></motion.div>
       </div>
     </div>
+   
   );
 };
 
@@ -72,4 +78,3 @@ export const Card = ({ card }) => {
 };
 
 export default Portfolio;
-
