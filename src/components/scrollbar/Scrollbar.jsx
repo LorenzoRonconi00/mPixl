00c7b27c25f2ffeb943fdeb8edc76dfb725eb4f1
Federicocervelli <federicocervelli01@gmail.com>
2023-10-30 13:00:43 +0100

    fix dot scrollbar

--- src/components/scrollbar/Scrollbar.jsx
+++ src/components/scrollbar/Scrollbar.jsx
@@ -1,45 +1,45 @@
+import { useEffect, useState } from "react";
 import "./scrollbar.scss";
 
 const Scrollbar = () => {
 
-    const dots = document.querySelectorAll(".scroll-indicator a");
-
-    const removeActiveClass = () => {
-        dots.forEach(dot => {
+  const changeActiveClass = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        let currentDot = document.querySelector(
+          `.scroll-indicator a[href='#${entry.target.id}']`
+        );
+        let allDots = document.querySelectorAll(".scroll-indicator a");
+        allDots.forEach((dot) => {
             dot.classList.remove("active");
-        })
-    }
+          });
+        currentDot.classList.add("active");
+      }
+    });
+  };
 
-    const addActiveClass = (entries, observer) => {
-        entries.forEach((entry) => {
-            if(entry.isIntersecting) {
-                let currentDot = document.querySelector(`.scroll-indicator a[href='#${entry.target.id}']`);
-                removeActiveClass();
-                currentDot.classList.add("active");
-            }
-        })
-    };
+  const options = {
+    threshold: 0.2,
+  };
 
-    const options = {
-        threshold: 0.2
-    };
-
-    const observer = new IntersectionObserver(addActiveClass, options);
+  useEffect(() => {
+    const observer = new IntersectionObserver(changeActiveClass, options);
 
     const sections = document.querySelectorAll("section");
 
-    sections.forEach(section => {
-        observer.observe(section);
-    })
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  }, []);
 
-    return ( 
-        <div className="scroll-indicator">
-            <a href="#Home" className="active" data-section="Home"></a>
-            <a href="#Servizi" data-section="Servizi"></a>
-            <a href="#Progetti" data-section="Progetti"></a>
-            <a href="#Contatti" data-section="Contatti"></a>
-        </div>
-     );
-}
- 
+  return (
+    <div className="scroll-indicator">
+      <a href="#Home" data-section="Home"></a>
+      <a href="#Servizi" data-section="Servizi"></a>
+      <a href="#Progetti" data-section="Progetti"></a>
+      <a href="#Contatti" data-section="Contatti"></a>
+    </div>
+  );
+};
+
 export default Scrollbar;
